601fc808c336611a12d2f9eb3f3bed52aafc45f5
petebromberg <pbromberg@gmail.com>
2021-04-25 16:20:25 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+
+namespace SQlBulkInsertDatatable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch s = new Stopwatch();
+            s.Start();
+            string path = @"C:\TEMP\cSun_tSSB_6000y_x5d.tsv\cSun_tSSB_6000y_x5d.csv";
+            DataTable dt = Convert.ConvertCSVToDataTable(path);
+            Task.Run(async () => await Execute(dt)).Wait();
+            s.Stop();
+            Console.WriteLine("Read and  inserted " + dt.Rows.Count.ToString() + " rows to database in " + s.ElapsedMilliseconds + " ms.");
+        }
+
+        public static async Task Execute(DataTable dt)
+        {
+            string connStr = ConfigurationManager.ConnectionStrings["Default"].ConnectionString;
+            System.Threading.Tasks.Task<int> t;
+            SqlConnection conn = new SqlConnection(connStr);
+            conn.Open();
+            try
+            {
+                using (SqlBulkCopy sbc =
+               new SqlBulkCopy(conn))
+                {
+                    sbc.DestinationTableName = "dbo.CSV";
+                    sbc.BulkCopyTimeout = 0;
+                    sbc.BatchSize = 100000;
+                    await sbc.WriteToServerAsync(dt);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+
+            }
+            
+        }
+    }
+         
+
+         
+    }
+
---
+++ ReadCSVToDatatable.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace SQlBulkInsertDatatable
+{
+    public static class Convert
+    {
+        public static DataTable ConvertCSVToDataTable(string strFilePath)
+        {
+            StreamReader sr = new StreamReader(strFilePath);
+            string[] headers = sr.ReadLine().Split(',');
+            DataTable dt = new DataTable();
+            foreach (string header in headers)
+            {
+                dt.Columns.Add(header);
+            }
+            while (!sr.EndOfStream)
+            {
+                string[] rows = Regex.Split(sr.ReadLine(), ",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
+                DataRow dr = dt.NewRow();
+                for (int i = 0; i < headers.Length; i++)
+                {
+                    dr[i] = rows[i];
+                }
+                dt.Rows.Add(dr);
+            }
+            return dt;
+        }
+    }
+}
