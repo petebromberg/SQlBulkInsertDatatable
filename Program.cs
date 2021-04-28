9ec7dc4d142d5ce8240b81fa9a54608df26e9ae7
petebromberg <pbromberg@gmail.com>
2021-04-28 12:59:49 -0400

    Reverted to working

--- Program.cs
+++ Program.cs
@@ -5,7 +5,6 @@ using System.Data.SqlClient;
 using System.Threading.Tasks;
 using System.Diagnostics;
 using System.IO;
-using System.Collections.Generic;
 
 namespace SQlBulkInsertDatatable
 {
@@ -15,69 +14,50 @@ namespace SQlBulkInsertDatatable
         static float insertTime = 0;
         static void Main(string[] args)
         {
-
-            Go();
-        }
-
-
-        static async void Go()
-        {
-
             Stopwatch s = new Stopwatch();
             s.Start();
             string path = @"C:\TEMP\cSun_tSSB_6000y_x5d.tsv\cSun_tSSB_6000y_x5d.csv";
             DataTable dt = Convert.ConvertCSVToDataTable(path);
             s.Stop();
             loadTime = s.ElapsedMilliseconds;
-
-            int rowsPerTable = dt.Rows.Count / 4;
-
-            // split up DataTable to 4 separa chunks for parallel testing
-            DataTable dt1 = new DataTable();
-            DataTable dt2 = new DataTable();
-            DataTable dt3 = new DataTable();
-            DataTable dt4 = new DataTable();
-            Convert.CopyRows(dt, dt1, 0, rowsPerTable);
-            Convert.CopyRows(dt, dt2, rowsPerTable, rowsPerTable * 2);
-            Convert.CopyRows(dt, dt3, rowsPerTable * 2, rowsPerTable * 3);
-            Convert.CopyRows(dt, dt4, rowsPerTable * 3, rowsPerTable * 4);
-
-
             s.Reset();
-            var tasks = new List<Task>();
-            tasks.Add(Task.Run(() => Execute(dt1)));
-            tasks.Add(Task.Run(() => Execute(dt2)));
-            tasks.Add(Task.Run(() => Execute(dt3)));
-            tasks.Add(Task.Run(() => Execute(dt4)));
-
             s.Start();
-           
-            Task t = Task.WhenAll(tasks);
-            try
-            {
-                t.Wait();
-            }
-            catch { }
-
+            Task.Run(async () => await Execute(dt)).Wait();
             s.Stop();
             insertTime = s.ElapsedMilliseconds;
             Console.WriteLine("Read in " + loadTime.ToString() + "ms  and  inserted " + dt.Rows.Count.ToString() + " rows to database in " + insertTime.ToString() + "ms.");
         }
 
+
+        /*public async Task Execute()
+{
+    using(var sourceConnection = new SqlConnection(_sourceConnectionString))
+    using(var targetConnection = new SqlConnection(_targetConnectionString))
+    using(var sourceCommand = new SqlCommand(_selectQuery, sourceConnection))
+    using(var bulkCopy = new SqlBulkCopy(targetConnection))
+    {
+        await Task.WhenAll(sourceConnection.OpenAsync(),
+                           targetConnection.OpenAsync());
  
+        var reader = await sourceCommand.ExecuteReaderAsync();
+ 
+        bulkCopy.DestinationTableName = _targetTable;
+        await bulkCopy.WriteToServerAsync(reader);
+    }
+}
+        */
 
         public static async Task Execute(DataTable dt)
         {
             string connStr = ConfigurationManager.ConnectionStrings["Default"].ConnectionString;
-          
+
             SqlConnection conn = new SqlConnection(connStr);
-            //features tablock
             conn.Open();
             try
             {
                 using (SqlBulkCopy sbc = new SqlBulkCopy(conn))
                 {
-                    
+
                     sbc.DestinationTableName = "dbo.CSV";
                     sbc.BulkCopyTimeout = 0;
                     sbc.BatchSize = dt.Rows.Count;
@@ -90,11 +70,10 @@ namespace SQlBulkInsertDatatable
                 Console.WriteLine(ex.Message);
 
             }
-            
+
         }
     }
-         
 
-         
-    }
 
+
+}
